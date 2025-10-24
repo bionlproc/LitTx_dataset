5a7427e67174641fedf5e072edee41245a3cbda7
yuhangjiang22 <74789310+yuhangjiang22@users.noreply.github.com>
2025-10-24 16:45:35 -0400

    Fix dataset path in train_args.py

--- train_args.py
+++ train_args.py
@@ -1,7 +1,7 @@
 import os
 os.system('python train.py \
     --output_dir <output_dir> \
-    --dataset_name drug-dataset/drug.py \
+    --dataset_name dataset/drug.py \
     --model_id meta-llama/Meta-Llama-3.1-8B-Instruct \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 1 \
